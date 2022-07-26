a697421a95eee7fbc3a95ac13246feede6264f28
swachaure <62742870+swachaure@users.noreply.github.com>
2022-07-26 22:57:18 +1000

    Game Over and re-run

--- MONOPOLY/.idea/sonarlint/issuestore/index.pb
+++ MONOPOLY/.idea/sonarlint/issuestore/index.pb
@@ -10,3 +10,18 @@ Rolls.java,f\7\f7e58b86c7393249e6138e1ddc1758f00d515f80
 Property.java,a\2\a2436a754cbd7a3461b88663fdc118f70fd43825
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+;
+Player.java,d\a\da3fb2dc548054d4304c81c4544744bcc3284bee
+:
+
+board.json,6\f\6f59722f5566d14fa2faae3abb3415494a8c8299
+<
+Utility.java,1\0\10f27964ad31f149c258b14eb30423bee6da75ec
+:
+
+Board.java,a\9\a91579153388110e980846d082e14ff294b1718f
+=
+Monopoly.java,8\b\8bd846bad5522282d495362cd298579d4ea1e34c
+:
+
+Output.txt,c\4\c47c6fb83cc7f835e81af45d91019388ec375010
--- MONOPOLY/Monopoly.java
+++ MONOPOLY/Monopoly.java
@@ -3,187 +3,203 @@ import java.util.Scanner;
 
 public class Monopoly {
 
-    private static final Board board = Board.getInstance();
-    private static final Rolls rolls = Rolls.getInstance();
+	private static Board board;
+	private static Rolls rolls;
 
-    private static Integer balanceOfPlayer1;
-    private static Integer balanceOfPlayer2;
-    private static Integer balanceOfPlayer3;
-    private static Integer balanceOfPlayer4;
+	private static Integer balanceOfPlayer1;
+	private static Integer balanceOfPlayer2;
+	private static Integer balanceOfPlayer3;
+	private static Integer balanceOfPlayer4;
 
-    public static void main(String[] args) throws IOException {
-        // initialize and load the board
-        init();
+	public static void main(String[] args) throws IOException {
+		// initialize and load the board
 
-        boolean stopFlag = false;
-        boolean welcomePrint = true;
-        do {
-            if (welcomePrint) {
-                Utility.printHash();
-                System.out.println();
-                System.out.println();
-                System.out.println("\t\t\t\t\tWelcome to monopoly game");
-                System.out.printf("\t\t Four player are loaded ");
-                Board.getInstance().printAllPlayersName();
-                System.out.println();
-                System.out.println("\t\t\t Board loaded with options like as below");
-                System.out.println();
-                Board.getInstance().printAllProperties();
-                Utility.printHash();
-                System.out.println("Your options are as below");
-                System.out.println("Simulate Game (Press 1 for start game)");
-            }
+		boolean stopFlag = false;
+		boolean welcomePrint = true;
+		do {
+//			board = Board.getInstance();
+			if (welcomePrint) {
+				Utility.printHash();
+				System.out.println();
+				System.out.println("\t\t\t\t\tWelcome to monopoly game");
+				System.out.println();
+				Utility.printHash();
+				System.out.println();
+			}
+			System.out.println("Your options are as below");
+			System.out.println("Simulate Game (Press 1 for simulate game)");
+			System.out.println("Exit from Game (Press 2 for exit game)");
+			Scanner sc = new Scanner(System.in);
+			String option = sc.next();
+			switch (option) {
+				case "1": {
+					board = new Board();
+					rolls = new Rolls();
 
-            System.out.println("Exit from Game (Press 2 for exit game)");
-            Scanner sc = new Scanner(System.in);
-            int option = sc.nextInt();
-            switch (option) {
-                case 1: {
-                    welcomePrint = false;
-                    System.out.println("You've Started the monopoly game");
-                    start();
-                }
-                break;
-                case 2: {
-                    welcomePrint = false;
-                    stopFlag = true;
-                    System.exit(0);
-                }
-                break;
-            }
-        } while (!stopFlag);
+					init();
+					Utility.printStar();
+					System.out.println("You've Started simulating the monopoly game");
+					System.out.printf("Four player are loaded ");
+					board.printAllPlayersName();
+					System.out.println();
+					System.out.println("Board loaded with spaces like as below");
+					board.printAllProperties();
+					Utility.printStar();
 
-    }
+					welcomePrint = false;
+					start();
+				}
+				break;
+				case "2": {
+					welcomePrint = false;
+					stopFlag = true;
+					System.exit(0);
+				}
+				break;
+				default:
+					welcomePrint = false;
+					Utility.printDash();
+					System.out.println("Please choose correct option");
+					Utility.printDash();
+					break;
+			}
+		} while (!stopFlag);
 
-    private static void init() throws IOException {
-        // add properties cards
-        board.addPropertiesToBoard();
+	}
 
-        Player peter = new Player("Peter");
-        Player billy = new Player("Billy");
-        Player charlotte = new Player("Charlotte");
-        Player sweedal = new Player("Sweedal");
+	private static void init() throws IOException {
+		// add properties cards
+		board.addPropertiesToBoard();
 
-        //add players start
-        board.addPlayer(peter);
-        board.addPlayer(billy);
-        board.addPlayer(charlotte);
-        board.addPlayer(sweedal);
-        //add players end
+		Player peter = new Player("Peter");
+		Player billy = new Player("Billy");
+		Player charlotte = new Player("Charlotte");
+		Player sweedal = new Player("Sweedal");
 
-        // load all rolls
-        Rolls rolls = Rolls.getInstance();
-        rolls.loadRolls();
+		//add players start
+		board.addPlayer(peter);
+		board.addPlayer(billy);
+		board.addPlayer(charlotte);
+		board.addPlayer(sweedal);
+		//add players end
 
-    }
+		// load all rolls
+		rolls.loadRolls();
 
-    private static void start() {
-        boolean init = true;
-        Utility.printDash();
-        for (int i = 1; i <= 48; i = i + 4) {
-            int player1 = i;
-            int player2 = player1 + 1;
-            int player3 = player2 + 1;
-            int player4 = player3 + 1;
+	}
 
-            int player1Dice1 = rolls.getAllRollsOfDice1().get(player1);
-            int player1Dice2 = rolls.getAllRollsOfDice2().get(player1);
+	private static void start() {
+		boolean init = true;
+		Utility.printDash();
+		for (int i = 1; i <= 48; i = i + 4) {
+			int player1 = i;
+			int player2 = player1 + 1;
+			int player3 = player2 + 1;
+			int player4 = player3 + 1;
 
-            int player2Dice1 = rolls.getAllRollsOfDice1().get(player2);
-            int player2Dice2 = rolls.getAllRollsOfDice2().get(player2);
+			int player1Dice1 = rolls.getAllRollsOfDice1().get(player1);
+			int player1Dice2 = rolls.getAllRollsOfDice2().get(player1);
 
-            int player3Dice1 = rolls.getAllRollsOfDice1().get(player3);
-            int player3Dice2 = rolls.getAllRollsOfDice2().get(player3);
+			int player2Dice1 = rolls.getAllRollsOfDice1().get(player2);
+			int player2Dice2 = rolls.getAllRollsOfDice2().get(player2);
 
-            int player4Dice1 = rolls.getAllRollsOfDice1().get(player4);
-            int player4Dice2 = rolls.getAllRollsOfDice2().get(player4);
+			int player3Dice1 = rolls.getAllRollsOfDice1().get(player3);
+			int player3Dice2 = rolls.getAllRollsOfDice2().get(player3);
 
-            int player1Total = (init) ? player1Dice1 + player1Dice2 + 1 : player1Dice1 + player1Dice2;
-            int player2Total = (init) ? (player2Dice1 + player2Dice2) + 1 : (player2Dice1 + player2Dice2);
-            int player3Total = (init) ? (player3Dice1 + player3Dice2) + 1 : (player3Dice1 + player3Dice2);
-            int player4Total = (init) ? (player4Dice1 + player4Dice2) + 1 : (player4Dice1 + player4Dice2);
-            init = false;
+			int player4Dice1 = rolls.getAllRollsOfDice1().get(player4);
+			int player4Dice2 = rolls.getAllRollsOfDice2().get(player4);
 
-            board.setCurrentPosOfPlayer1(player1Total);
-            board.setCurrentPosOfPlayer2(player2Total);
-            board.setCurrentPosOfPlayer3(player3Total);
-            board.setCurrentPosOfPlayer4(player4Total);
+			int player1Total = (init) ? player1Dice1 + player1Dice2 + 1 : player1Dice1 + player1Dice2;
+			int player2Total = (init) ? (player2Dice1 + player2Dice2) + 1 : (player2Dice1 + player2Dice2);
+			int player3Total = (init) ? (player3Dice1 + player3Dice2) + 1 : (player3Dice1 + player3Dice2);
+			int player4Total = (init) ? (player4Dice1 + player4Dice2) + 1 : (player4Dice1 + player4Dice2);
+			init = false;
 
-            String currentPositionOfPlayer1 = board.getPropertiesMap().get(board.getCurrentPosOfPlayer1()).getName();
-            String currentPositionOfPlayer2 = board.getPropertiesMap().get(board.getCurrentPosOfPlayer2()).getName();
-            String currentPositionOfPlayer3 = board.getPropertiesMap().get(board.getCurrentPosOfPlayer3()).getName();
-            String currentPositionOfPlayer4 = board.getPropertiesMap().get(board.getCurrentPosOfPlayer4()).getName();
+			board.setCurrentPosOfPlayer1(player1Total);
+			board.setCurrentPosOfPlayer2(player2Total);
+			board.setCurrentPosOfPlayer3(player3Total);
+			board.setCurrentPosOfPlayer4(player4Total);
 
-            balanceOfPlayer1 = board.getBalance("Peter");
-            balanceOfPlayer2 = board.getBalance("Billy");
-            balanceOfPlayer3 = board.getBalance("Charlotte");
-            balanceOfPlayer4 = board.getBalance("Sweedal");
+			String currentPositionOfPlayer1 = board.getPropertiesMap().get(board.getCurrentPosOfPlayer1()).getName();
+			String currentPositionOfPlayer2 = board.getPropertiesMap().get(board.getCurrentPosOfPlayer2()).getName();
+			String currentPositionOfPlayer3 = board.getPropertiesMap().get(board.getCurrentPosOfPlayer3()).getName();
+			String currentPositionOfPlayer4 = board.getPropertiesMap().get(board.getCurrentPosOfPlayer4()).getName();
 
-            System.out.println("Player 1's Turn ( " + player1 + " ) Dice 1 ( " + player1Dice1 + " ) " + "Dice 2 ( " + player1Dice2 + " )" + " Total ( " + player1Total + " )" + " Current Position ( " + currentPositionOfPlayer1 + " )" + " Peter's Balance ( " + balanceOfPlayer1 + " )");
-            System.out.println("Player 2's Turn ( " + player2 + " ) Dice 1 ( " + player2Dice1 + " ) " + "Dice 2 ( " + player2Dice2 + " )" + " Total ( " + player2Total + " )" + " Current Position ( " + currentPositionOfPlayer2 + " )" + " Billy's Balance ( " + balanceOfPlayer2 + " )");
-            System.out.println("Player 3's Turn ( " + player3 + " ) Dice 1 ( " + player3Dice1 + " ) " + "Dice 2 ( " + player3Dice2 + " )" + " Total ( " + player3Total + " )" + " Current Position ( " + currentPositionOfPlayer3 + " )" + " Charlotte's Balance ( " + balanceOfPlayer3 + " )");
-            System.out.println("Player 4's Turn ( " + player4 + " ) Dice 1 ( " + player4Dice1 + " ) " + "Dice 2 ( " + player4Dice2 + " )" + " Total ( " + player4Total + " )" + " Current Position ( " + currentPositionOfPlayer4 + " )" + " Sweedal's Balance ( " + balanceOfPlayer4 + " )");
+			balanceOfPlayer1 = board.getBalance("Peter");
+			balanceOfPlayer2 = board.getBalance("Billy");
+			balanceOfPlayer3 = board.getBalance("Charlotte");
+			balanceOfPlayer4 = board.getBalance("Sweedal");
 
-            if (balanceOfPlayer1 <= 0 || balanceOfPlayer2 <= 0 || balanceOfPlayer3 <= 0 || balanceOfPlayer4 <= 0) {
-                printFooter();
-                System.exit(0);
-            }
-            Utility.printDash();
-        }
-    }
+			System.out.println("Player 1's Turn ( " + player1 + " ) Dice 1 ( " + player1Dice1 + " ) " + "Dice 2 ( " + player1Dice2 + " )" + " Total ( " +
+					player1Total + " )" + " Current Position ( " + currentPositionOfPlayer1 + " )" + " Peter's Balance ( " + balanceOfPlayer1 + " )");
+			System.out.println("Player 2's Turn ( " + player2 + " ) Dice 1 ( " + player2Dice1 + " ) " + "Dice 2 ( " + player2Dice2 + " )" + " Total ( " +
+					player2Total + " )" + " Current Position ( " + currentPositionOfPlayer2 + " )" + " Billy's Balance ( " + balanceOfPlayer2 + " )");
+			System.out.println("Player 3's Turn ( " + player3 + " ) Dice 1 ( " + player3Dice1 + " ) " + "Dice 2 ( " + player3Dice2 + " )" + " Total ( " +
+					player3Total + " )" + " Current Position ( " + currentPositionOfPlayer3 + " )" + " Charlotte's Balance ( " + balanceOfPlayer3 +
+					" )");
+			System.out.println("Player 4's Turn ( " + player4 + " ) Dice 1 ( " + player4Dice1 + " ) " + "Dice 2 ( " + player4Dice2 + " )" + " Total ( " +
+					player4Total + " )" + " Current Position ( " + currentPositionOfPlayer4 + " )" + " Sweedal's Balance ( " + balanceOfPlayer4 + " )");
 
-    private static void printFooter() {
-        Utility.printHash();
-        Utility.printStar();
-        System.out.println();
-        System.out.println("\t\t\t\t\t\tGame Completed");
-        System.out.println("\t\t\t\t\t\t--------------");
-        System.out.println();
+			if (balanceOfPlayer1 <= 0 || balanceOfPlayer2 <= 0 || balanceOfPlayer3 <= 0 || balanceOfPlayer4 <= 0) {
+				printFooter();
+//                System.exit(0);
+			}
+			Utility.printDash();
+		}
+	}
 
-        System.out.println("\t\t\t* * *  1. Peter's Balance ( " + balanceOfPlayer1 + " )\t \t * * * ");
-        System.out.println("\t\t\t* * *  2. Billy's Balance ( " + balanceOfPlayer2 + " ) \t * * * ");
-        System.out.println("\t\t\t* * *  3. Charlotte's Balance ( " + balanceOfPlayer3 + " ) * * * ");
-        System.out.println("\t\t\t* * *  4. Sweedal's Balance ( " + balanceOfPlayer4 + " )\t * * * ");
-        System.out.println();
+	private static void printFooter() {
+		Utility.printHash();
+		Utility.printStar();
+		System.out.println();
+		System.out.println("\t\t\t\t\t\tGame Over");
+		System.out.println("\t\t\t\t\t\t--------------");
+		System.out.println();
 
-        board.getPlayers().get("Peter").printAllOwnedProperties();
-        board.getPlayers().get("Billy").printAllOwnedProperties();
-        board.getPlayers().get("Charlotte").printAllOwnedProperties();
-        board.getPlayers().get("Sweedal").printAllOwnedProperties();
-        System.out.println();
+		System.out.println("\t\t\t* * *  1. Peter's Balance ( " + balanceOfPlayer1 + " )\t \t * * * ");
+		System.out.println("\t\t\t* * *  2. Billy's Balance ( " + balanceOfPlayer2 + " ) \t * * * ");
+		System.out.println("\t\t\t* * *  3. Charlotte's Balance ( " + balanceOfPlayer3 + " ) * * * ");
+		System.out.println("\t\t\t* * *  4. Sweedal's Balance ( " + balanceOfPlayer4 + " )\t * * * ");
+		System.out.println();
 
-        if (balanceOfPlayer1 <= 0) {
-            System.out.println("***************************** Peter is bankrupt *****************************");
-        }
-        if (balanceOfPlayer2 <= 0) {
-            System.out.println("***************************** Billy is bankrupt *****************************");
-        }
-        if (balanceOfPlayer3 <= 0) {
-            System.out.println("***************************** Charlotte is bankrupt *****************************");
-        }
-        if (balanceOfPlayer4 <= 0) {
-            System.out.println("***************************** Sweedal is bankrupt *****************************");
-        }
+		board.getPlayers().get("Peter").printAllOwnedProperties();
+		board.getPlayers().get("Billy").printAllOwnedProperties();
+		board.getPlayers().get("Charlotte").printAllOwnedProperties();
+		board.getPlayers().get("Sweedal").printAllOwnedProperties();
+		System.out.println();
 
-        int winner = balanceOfPlayer1;
-        String winnerPlayer = "Peter";
-        if (balanceOfPlayer2 > winner) {
-            winnerPlayer = "Billy";
-            winner = balanceOfPlayer2;
-        }
-        if (balanceOfPlayer3 > winner) {
-            winnerPlayer = "Charlotte";
-            winner = balanceOfPlayer3;
-        }
-        if (balanceOfPlayer4 > winner) {
-            winnerPlayer = "Billy";
-            winner = balanceOfPlayer4;
-        }
+		if (balanceOfPlayer1 <= 0) {
+			System.out.println("***************************** Peter is bankrupt *****************************");
+		}
+		if (balanceOfPlayer2 <= 0) {
+			System.out.println("***************************** Billy is bankrupt *****************************");
+		}
+		if (balanceOfPlayer3 <= 0) {
+			System.out.println("***************************** Charlotte is bankrupt *****************************");
+		}
+		if (balanceOfPlayer4 <= 0) {
+			System.out.println("***************************** Sweedal is bankrupt *****************************");
+		}
 
-        System.out.println(" | *** " + winnerPlayer + " *** | With Balance ( " + winner + " ) Is Game Winner");
+		int winner = balanceOfPlayer1;
+		String winnerPlayer = "Peter";
+		if (balanceOfPlayer2 > winner) {
+			winnerPlayer = "Billy";
+			winner = balanceOfPlayer2;
+		}
+		if (balanceOfPlayer3 > winner) {
+			winnerPlayer = "Charlotte";
+			winner = balanceOfPlayer3;
+		}
+		if (balanceOfPlayer4 > winner) {
+			winnerPlayer = "Billy";
+			winner = balanceOfPlayer4;
+		}
 
-        System.out.println();
-        Utility.printStar();
-        Utility.printHash();
-    }
+		System.out.println(" | *** " + winnerPlayer + " *** | With Balance ( " + winner + " ) Is Game Winner");
+
+		System.out.println();
+		Utility.printStar();
+		Utility.printHash();
+	}
 }
